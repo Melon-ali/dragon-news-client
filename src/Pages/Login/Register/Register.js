1c36caafa883f3fb4d0b00d94d6c1061151b56f1
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-04-26 10:37:16 +0600

    dragon news added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
 
+    const {createUser} = useContext(AuthContext);
+
     const handleSubmit = event => {
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoURL, email, password);
+
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+        })
+        .catch(error => {
+            console.error(error)
+        })
     }
 
     return (
@@ -17,7 +36,7 @@ const Register = () => {
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Photo URL</Form.Label>
-                <Form.Control name='PhotoURL' type="email" placeholder="Your PhotoURL" />
+                <Form.Control name='photoURL' type="text" placeholder="Your PhotoURL" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
